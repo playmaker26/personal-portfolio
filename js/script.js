5ebb407a78967e6f680b961c382fdc2a405daa75
playmaker26 <jorge.artaza22@gmail.com>
2024-12-14 14:33:24 -0500

    completed project

--- js/script.js
+++ js/script.js
@@ -45,10 +45,46 @@ function animation() {
     project.forEach(projects => {
         observer.observe(projects);
     });
+    const contactInfo = document.querySelector('.contact-info');
+    observer.observe(contactInfo);
+    const contactForm = document.querySelector('.contact');
+    observer.observe(contactForm);
 }
 animation();
 
 let formValidation = function() {
+let name = document.querySelector('#name')
+let subject = document.querySelector('#subject');
+let email = document.querySelector('#email');
+let message = document.querySelector('#message');
+let form = document.querySelector('.contact');
+
+form.addEventListener('submit', (e) =>{
+   
+    if(name.value === '' || name.value === null) {
+        document.querySelector('.name-label').style.color = '#FF0000';
+        document.querySelector('.name-label').textContent = 'Type your name';
+        e.preventDefault();
+    }
+
+    if(subject.value === '' || subject.value === null) {
+        document.querySelector('.subject-label').style.color = '#FF0000';
+        document.querySelector('.subject-label').textContent = 'Type your a subject';
+        e.preventDefault();
+    }
+
+    if(email.value === '' || email.value === null) {
+        document.querySelector('.email-label').style.color = '#FF0000';
+        document.querySelector('.email-label').textContent = 'Type your E-mail';
+        e.preventDefault();
+    }
+
+    if (message.value === '' || message.value === null) {
+        document.querySelector('.message-label').style.color = '#FF0000';
+        document.querySelector('.message-label').textContent = 'Type your message';
+        e.preventDefault();
+    }
+});
 
 }
 formValidation();
